393ad6544e36b5977b98a23059552694c43c027c
Fun-25 <muskandodmani760@gmail.com>
2023-06-21 10:48:08 +0530

    submitting code-Wed Jun 21 2023 10:48:05 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,51 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedOut, setIsLoggedOut] = useState(true);
+
+  const Child = ({ logStatus }) => {
+    const [count, setCount] = useState(0);
+
+    function handleChange() {
+      if (count > 2) setIsLoggedIn(true);
+    }
+    return (
+      <div className="child">
+        <form>
+          <div>
+            <label>Username:</label>
+            <input
+              type="text"
+              onKeyDown={() => setCount((prevcount) => prevcount + 1)}
+            />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input
+              type="password"
+              onKeyDown={() => setCount((prevcount) => prevcount + 1)}
+            />
+          </div>
+          <div>
+            <button onClick={() => handleChange()}>Login</button>
+          </div>
+        </form>
+      </div>
+    );
+  };
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="parent">
+      <h1>Parent Component</h1>
+      {isLoggedIn ? (
+        <p>You are logged in!</p>
+      ) : (
+        isLoggedOut && <Child logStatus={setIsLoggedIn} />
+      )}
     </div>
-  )
+  );
 }
 
 export default App
